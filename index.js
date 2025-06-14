11d87f2390be760b7439c8cea1e24455802afcd5
Aarush Gupta <aarushgupta2018@gmail.com>
2025-06-14 12:54:49 +0530

    hagisdhusa

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = 5001;
 
 // ✅ Corrected CORS config
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://performance-tool-frontend-git-main-aarush18s-projects.vercel.app'],
+  origin: ['http://localhost:3000', 'https://performance-tool-frontend-kpfkelqqo-aarush18s-projects.vercel.app'],
   credentials: true
 }));
 
